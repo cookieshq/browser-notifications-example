12d74edeb8ac04a115f22a6d3a590fb62d12fc4d
Rob Paskin <rjpaskin+git@gmail.com>
2016-07-27 12:37:58 +0100

    Use Bootstrap classes for flashes

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,5 +1,13 @@
 module FlashesHelper
+  FLASH_CLASSES = {
+    "alert" => "warning",
+    "notice" => "info"
+  }
   def user_facing_flashes
     flash.to_hash.slice("alert", "error", "notice", "success")
+  end
+
+  def flash_class(key)
+    FLASH_CLASSES[key]
   end
 end
