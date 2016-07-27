9c1389c7d7fef7a3f607d2ec5261686d2bb9badc
Rob Paskin <rjpaskin+git@gmail.com>
2016-07-27 11:29:10 +0100

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,18 @@
+require_relative 'boot'
+require 'rails/all'
+Bundler.require(*Rails.groups)
+module BrowserNotificationsExample
+  class Application < Rails::Application
+    config.assets.quiet = true
+    config.generators do |generate|
+      generate.helper false
+      generate.javascript_engine false
+      generate.request_specs false
+      generate.routing_specs false
+      generate.stylesheets false
+      generate.test_framework :rspec
+      generate.view_specs false
+    end
+    config.action_controller.action_on_unpermitted_parameters = :raise
+  end
+end
