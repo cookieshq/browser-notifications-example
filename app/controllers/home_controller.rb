1dd16e45ff82212dfe8131042e84b812f7848665
Rob Paskin <rjpaskin+git@gmail.com>
2016-07-27 14:56:02 +0100

    Add home#manifest JSON endpoint

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,13 @@ class HomeController < ApplicationController
       @account = Account.find_by!(pin: cookies[:account_pin])
     end
   end
+
+  def manifest
+    manifest_hash = {
+      name: "Browser notifications example",
+      gcm_sender_id: ENV["GCM_SENDER_ID"]
+    }
+
+    render json: manifest_hash
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :accounts, only: [:create]
   resources :connections, only: [:new, :create]
 
+  get "manifest" => "home#manifest", constraints: { format: :json }
+
   root to: "home#index"
 end
